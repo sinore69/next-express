e9213836aca847943e04a5245b76a7e16a3781cf
saptarshi dutta <saptarshidutta599@gmail.com>
2023-08-28 13:45:02 +0530

    adding types

--- client/my-app/src/components/Comments.tsx
+++ client/my-app/src/components/Comments.tsx
@@ -2,19 +2,21 @@
 import React from 'react'
 import {useQuery} from '@tanstack/react-query'
 import { getcomments } from '@/app/_actions/getcomments'
+import { commentList } from '@/types'
 function Comments() {
 const comm=useQuery({
     queryKey:['comments'],
-    queryFn:()=>{
-        const comments= getcomments();
-        console.log(comments)
+    queryFn:async()=>{
+        const comments= await getcomments();
         return comments;
     }
 })
   return (
     <div>
 {
-
+comm?.data?.map((item:commentList)=>(
+  <p key={item.id}>{item.post}</p>
+))
 }
     </div>
   )
--- client/my-app/src/types.ts
+++ client/my-app/src/types.ts
@@ -1,3 +1,8 @@
 export type comment={
     comment:string,
   }
+
+export type commentList={
+  id:number,
+  post:string
+}
